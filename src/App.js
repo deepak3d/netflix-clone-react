88be919a8c59786a3aa0e6823f128284a49c4d6b
deBug <deepakgupta.3d@gmail.com>
2021-01-18 02:35:49 +0530

    project complete

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
-
+import Banner from "./Banner";
+import Nav from "./Nav";
 function App() {
   return (
     <div className="App">
-      <h1> this first project </h1> 
-      <Row title="Netflix Originals" fetctUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Treding Now" fetctUrl={requests.fetchTrending}/>
+      {/* nav */}
+      <Nav />
+      {/* banner */}
+      <Banner />
+      {/* <h1> this first project </h1>  */}
+      <Row title="NETFLIX Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
+      <Row title="Treding Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="TopRated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comdey Movies" fetchUrl={requests.fetchComdeyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaies" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
-
 export default App;
--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3'
+    // https://api.themoviedb.org/3/movie/550?api_key=b97978517b614d256ae0df084598757c
 });
 
 // instance.get('/foo-bar');
